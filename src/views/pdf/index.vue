6f2f9faeca81e40e7fdd2f116af49331ce4df4d2
wll <appleWanglinlin@163.com>
2020-11-22 22:42:15 +0800

    视频播放

--- src/views/pdf/index.vue
+++ src/views/pdf/index.vue
@@ -7,6 +7,39 @@
       </el-button>
     </router-link>
     <aside style="margin-top:15px;">pdf</aside>
+    <aside style="margin-top:15px;">video</aside>
+    <video
+      ref="video"
+      controls
+      src="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-learning-vue/52d32740-aecd-11ea-b244-a9f5e5565f30.mp4">
+    </video>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      let myVideo = this.$refs.video
+      // console.dir(myVideo)
+      // 让视频观看到第10s
+      myVideo.currentTime = 10;
+      //获取视频的总长度
+      // console.log(myVideo.duration)
+    })
+  }
+}
+</script>
+
+<style scoped>
+  video {
+    width: 100%;
+    height: 100%;
+  }
+</style>
 
